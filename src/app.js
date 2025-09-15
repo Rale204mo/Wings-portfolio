db2a95aa4fc33ffe3b2cc9fe5a873bfe4a6f70e5
Motsamai Ralejoe <motsamairalejoe@gmail.com>
2025-09-15 17:48:37 +0200

    Wings Cafe initial commit

--- src/app.js
+++ src/app.js
@@ -9,6 +9,14 @@ import Stock from "./components/Stock/Stock";
 import Reports from "./components/Reports/Reports";
 
 function App() {
+
+  useEffect(() => {
+    // 👇 Replace with your Render backend URL
+    fetch("https://wings-cafe-2-cbps.onrender.com/api/data")
+      .then((res) => res.json())
+      .then((result) => setData(result))
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
   return (
     <div>
       <Header />
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./index.css"; // optional if you have global styles
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>
